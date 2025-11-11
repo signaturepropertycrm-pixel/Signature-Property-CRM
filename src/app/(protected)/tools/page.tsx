a7803e4ef756df9bfd7b9c2a17680661e3042404
Signature Property CRM <signaturepropertycrm@gmail.com>
2025-11-11 11:56:31 +0000

    nhi yar rehne do deal tracker khatam krdo

--- src/app/(protected)/tools/page.tsx
+++ src/app/(protected)/tools/page.tsx
@@ -1,15 +1,9 @@
-
 'use client';
 
 import { ListGeneratorTool } from '@/components/list-generator-tool';
 import { properties as initialProperties } from '@/lib/data';
 import { Property } from '@/lib/types';
 import { useEffect, useState } from 'react';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
-import { DealTrackerTool } from '@/components/deal-tracker-tool';
-import { List, Calculator } from 'lucide-react';
-
 
 export default function ToolsPage() {
   const [properties, setProperties] = useState<Property[]>([]);
@@ -34,24 +28,7 @@ export default function ToolsPage() {
         </p>
       </div>
 
-       <Tabs defaultValue="list-generator">
-        <TabsList className="grid w-full grid-cols-2 max-w-md">
-          <TabsTrigger value="list-generator">
-            <List className="mr-2 h-4 w-4" />
-            List Generator
-            </TabsTrigger>
-          <TabsTrigger value="deal-tracker">
-            <Calculator className="mr-2 h-4 w-4" />
-            Deal Tracker
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="list-generator">
-          <ListGeneratorTool allProperties={properties} />
-        </TabsContent>
-        <TabsContent value="deal-tracker">
-          <DealTrackerTool />
-        </TabsContent>
-      </Tabs>
+      <ListGeneratorTool allProperties={properties} />
     </div>
   );
 }
