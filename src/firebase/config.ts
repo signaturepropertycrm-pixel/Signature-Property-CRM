1279abd114a31c45e8f6b6799e8658ba9df51cc4
Signature Property CRM <signaturepropertycrm@gmail.com>
2025-11-19 14:57:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1155330597-951c7",
+  "appId": "1:813147604041:web:1d2f1e026017c9d704ab98",
+  "apiKey": "AIzaSyBtplxzDV9eT3oFHT2mw37J-sHW-_eiVcc",
+  "authDomain": "studio-1155330597-951c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "813147604041"
+};
