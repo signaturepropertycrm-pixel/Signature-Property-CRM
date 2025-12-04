6435d8a0d77518682302c4ae74f900d496cd7c32
Signature Property CRM <signaturepropertycrm@gmail.com>
2025-12-04 12:49:03 +0000

    yar baat suno ye jo Dropdown Menu ha ye jab open ho to ik Khubsurat tare

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,11 +1,31 @@
 "use client"
 
+import * as React from "react"
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+import { cn } from "@/lib/utils"
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <CollapsiblePrimitive.Content
+    ref={ref}
+    className={cn(
+      "overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
+      className
+    )}
+    {...props}
+  >
+    <div className="pt-1">{children}</div>
+  </CollapsiblePrimitive.Content>
+))
+
+CollapsibleContent.displayName = CollapsiblePrimitive.Content.displayName
+
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
